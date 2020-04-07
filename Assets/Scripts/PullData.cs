cd8d2b0a00d7b07829fc92412a1c7fe2671d29a8
Adrian Vaduva <adrian.vaduva98@gmail.com>
2020-04-07 15:43:02 +0100

    Scene 2
    Created the txt updating scripts, addeed tag along script, added a button for future use.

---
+++ Assets/Scripts/PullData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullData : MonoBehaviour
+{
+    public string name;
+    public string status;
+    public string liveData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke GEtData after 1 sec, every 5 seconds
+        InvokeRepeating("GetData", 1.0f, 5.0f);        
+    }
+
+
+
+    //Used to extract data
+    void GetData()
+    {
+        //TO DO
+        //store the data in the public variables defined at the beginning as STRINGS
+        //will use those to change the text 
+    }
+}
---
+++ Assets/Scripts/TextInsert.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextInsert : MonoBehaviour
+{
+    //Read the data from the display  
+    private void UpdateTextComponent()
+    {
+        this.gameObject.GetComponent<TextMeshPro>().text = GameObject.Find("Display").GetComponent<PullData>().name;
+    }
+
+    public void UpdateAllText()
+    {
+        GameObject.Find("NamedComponent").GetComponent<TextMeshPro>().text = GameObject.Find("Display").GetComponent<PullData>().name;
+        GameObject.Find("StatusComponent").GetComponent<TextMeshPro>().text = GameObject.Find("Display").GetComponent<PullData>().status;
+        GameObject.Find("LiveDataComponent").GetComponent<TextMeshPro>().text = GameObject.Find("Display").GetComponent<PullData>().liveData;
+    }
+
+    private void Update()
+    {
+        UpdateAllText();
+    }
+}
