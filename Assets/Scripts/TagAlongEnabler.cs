a18d2d7747610a7ac5c50ebc549116e9fd029bd7
Adrian Vaduva <adrian.vaduva98@gmail.com>
2020-04-14 14:11:49 +0100

    Tag Along enabled

---
+++ Assets/Scripts/TagAlongEnabler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+public class TagAlongEnabler : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    public void EnableOrDisable()
+    {
+        gameObject.GetComponent<RadialView>().enabled = !gameObject.GetComponent<RadialView>().enabled;
+    }
+}
