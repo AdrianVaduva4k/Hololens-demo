9e7c382eb50bdfa72feacf35a560606551b6e9d3
Adrian Vaduva <adrian.vaduva98@gmail.com>
2020-04-15 12:36:07 +0100

    TagAlongEnabler: Resets position and orientation

--- Assets/Scripts/TagAlongEnabler.cs
+++ Assets/Scripts/TagAlongEnabler.cs
@@ -6,9 +6,24 @@ using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 public class TagAlongEnabler : MonoBehaviour
 {
     public GameObject gameObject;
+    private Vector3 position;
+    private Quaternion rotation;
 
     public void EnableOrDisable()
     {
+        //Tag Along initial disabled
+        if (!gameObject.GetComponent<RadialView>().enabled)
+        {
+            //Get initial position and rotation
+            position = gameObject.transform.position;
+            rotation = gameObject.transform.rotation;
+        }
+        else
+        {
+            //Reset position
+            gameObject.transform.position = position;
+            gameObject.transform.rotation = rotation;
+        }
         gameObject.GetComponent<RadialView>().enabled = !gameObject.GetComponent<RadialView>().enabled;
     }
 }
